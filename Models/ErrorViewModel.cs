1711841619a318ef47f1c5169f496c307ec23fe9
primordialsocks <primordialsocks@gmail.com>
2018-08-06 21:46:37 -0500

    Adding project files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace lily.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
